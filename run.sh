3ab596013a10b0b5a45914534707155419d625a1
root <root@localhost.localdomain>
2017-04-09 17:05:22 +0800

    update run name

--- run.sh
+++ run.sh
@@ -2,13 +2,16 @@
 
 REPO=nothingdocker
 IMAGE_NAME=`basename $PWD`
-DOCKER_NAME=${1:-demo}
+DOCKER_NAME=${1:-nserver}
 docker rm -f $DOCKER_NAME
 docker run -d --privileged \
 	--name $DOCKER_NAME \
 	-v /sys/fs/cgroup:/sys/fs/cgroup:ro \
 	-v /data/mongo/db:/var/lib/mongo \
 	-v /data/mongo/logs:/var/log/mongodb \
+	-v /data:/data \
 	-p 127.0.0.1:23000:3000 \
+	-p 15672:15672 \
+	-p 5672:5672 \
 	$REPO/$IMAGE_NAME
 docker exec -it $DOCKER_NAME bash
